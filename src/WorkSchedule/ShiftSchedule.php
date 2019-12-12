9b4d49c5bd1300b8fa7f172bc1e82dcb99f825b6
v.zaica <v.zaica@wippo.md>
2019-12-12 22:59:51 +0200

    init

---
+++ src/WorkSchedule/ShiftSchedule.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace WorkSchedule;
+
+class ShiftSchedule
+{
+    private $work_days_count;
+    private $holiday_days_count;
+    private $start_work_date;
+
+    public function __construct($work_days_count, $holiday_days_count, $start_work_date)
+    {
+        $this->setWorkDaysCount($work_days_count);
+        $this->setHolidayDaysCount($holiday_days_count);
+        $this->setStartWorkDate($start_work_date);
+    }
+
+    public function getCarbonDate($date)
+    {
+        if ($date instanceof \Carbon\Carbon) {
+            return $date;
+        }
+
+        return \Carbon\Carbon::createFromFormat('Y-m-d', $date);
+    }
+
+    public function countWeekDays()
+    {
+        return $this->work_days_count + $this->holiday_days_count;
+    }
+
+    public function getDayNumInWeek($date)
+    {
+        $date = $this->getCarbonDate($date);
+
+        $days_count = $date->diffInDays($this->start_work_date) + 1;
+
+        $dayNum = $days_count % $this->countWeekDays();
+
+        if ($dayNum < 1) {
+            $dayNum = $this->countWeekDays();
+        }
+
+        return $dayNum;
+    }
+
+    public function isWorkDay($date)
+    {
+        $date = $this->getCarbonDate($date);
+
+        $dayInWeek = $this->getDayNumInWeek($date);
+
+        return $dayInWeek / $this->work_days_count <= 1;
+    }
+
+    public function isHoliday($date)
+    {
+        return !$this->isWorkDay($date);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWorkDaysCount()
+    {
+        return $this->work_days_count;
+    }
+
+    /**
+     * @param mixed $work_days_count
+     */
+    public function setWorkDaysCount($work_days_count)
+    {
+        $this->work_days_count = $work_days_count;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHolidayDaysCount()
+    {
+        return $this->holiday_days_count;
+    }
+
+    /**
+     * @param mixed $holiday_days_count
+     */
+    public function setHolidayDaysCount($holiday_days_count)
+    {
+        $this->holiday_days_count = $holiday_days_count;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartWorkDate()
+    {
+        return $this->start_work_date->format('Y-m-d');
+    }
+
+    /**
+     * @param mixed $start_work_date
+     */
+    public function setStartWorkDate($start_work_date)
+    {
+        $this->start_work_date = $this->getCarbonDate($start_work_date);
+    }
+}
---
+++ tests/WorkSchedule/ShiftScheduleTest.php
@@ -0,0 +1,45 @@
+<?php
+
+class ShiftScheduleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testGetTest()
+    {
+        $shiftSchedule = new WorkSchedule\ShiftSchedule(2, 2, '2019-12-12');
+
+        $this->assertTrue($shiftSchedule->isWorkDay('2019-12-12'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2019-12-13'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2019-12-14'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2019-12-15'));
+
+        $this->assertTrue($shiftSchedule->isWorkDay('2019-12-16'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2019-12-17'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2019-12-18'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2019-12-19'));
+
+        $shiftSchedule = new WorkSchedule\ShiftSchedule(3, 2, '2019-12-12');
+
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-01'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-02'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-03'));
+        $this->assertTrue($shiftSchedule->isHoliday('2020-03-04'));
+        $this->assertTrue($shiftSchedule->isHoliday('2020-03-05'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-06'));
+
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-08-08'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-08-09'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-08-10'));
+        $this->assertTrue($shiftSchedule->isHoliday('2020-08-11'));
+        $this->assertTrue($shiftSchedule->isHoliday('2020-08-12'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-08-13'));
+
+        $shiftSchedule = new WorkSchedule\ShiftSchedule(3, 3, '2019-12-12');
+
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-04-04'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-04-05'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-04-06'));
+        $this->assertTrue($shiftSchedule->isHoliday('2020-04-07'));
+        $this->assertTrue($shiftSchedule->isHoliday('2020-04-08'));
+        $this->assertTrue($shiftSchedule->isHoliday('2020-04-09'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-04-10'));
+    }
+}
