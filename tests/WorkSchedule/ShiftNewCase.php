92e999d5e7d240b89d43273beb06c1f51cae5ac7
nae1 <andreigarbatovschi@gmail.com>
2020-03-26 15:43:45 +0200

    Zile lucratoare si zilele de odihna pina ce se incepe perioada de lucru with holiday date and Sunday

--- tests/WorkSchedule/ShiftNewCase.php
+++ tests/WorkSchedule/ShiftNewCase.php
@@ -41,7 +41,30 @@ class ShiftNewCase extends \PHPUnit\Framework\TestCase
         $this->assertTrue($shiftSchedule->isWorkDay('2020-03-26'));
 
 
+    }
 
 
+    public function testHolidays()
+    {
+        $shiftSchedule = new WorkSchedule\ShiftSchedule(3, 3, '2020-03-25');
+
+        $shiftSchedule->setWeekend(WorkSchedule\ShiftSchedule::SUNDAY, true);
+
+
+        $shiftSchedule->setHoliday('2020-03-24', true);
+
+
+
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-17'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-18'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-19'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2020-03-20'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2020-03-21'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2020-03-22'));
+
+        $this->assertFalse($shiftSchedule->isWorkDay('2020-03-23'));
+        $this->assertFalse($shiftSchedule->isWorkDay('2020-03-24'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-25'));
+        $this->assertTrue($shiftSchedule->isWorkDay('2020-03-26'));
     }
 }
